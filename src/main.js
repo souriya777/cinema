0005e7fa3c44090b475c2a2a4fee322d62cb55b0
Souriya Phongsavanh <souriya.phongsavanh777@gmail.com>
2020-03-26 09:10:55 +0100

    save

---
+++ src/main.js
@@ -0,0 +1,66 @@
+import Vue from "vue";
+import "./style.scss";
+import genres from './util/genres';
+
+new Vue({
+  el: "#app",
+  components: {
+    "movie-list": {
+      template: `
+        <div id="movie-list">
+          <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+        </div>
+      `,
+      data: function() {
+        return {
+          movies: [
+            { title: "Pulp Fiction" },
+            { title: "Home Alone" },
+            { title: "Austin Powers" },
+          ]
+        }
+      }
+    },
+    "movie-filter": {
+      data: function() {
+        return {
+          genres
+        }
+      },
+      template: `
+        <div id="movie-filter">
+          <h2>Filter results</h2>
+          <div class="filter-group">
+            <check-filter
+              v-for="genre in genres"
+              v-bind:title="genre"
+            >
+              {{ genre }}
+            </check-filter>
+          </div>
+        </div>
+      `,
+      components: {
+        "check-filter": {
+          data: function() {
+            return {
+              checked: false
+            }
+          },
+          props: [
+            "title"
+          ],
+          template: `
+            <div 
+              :class="{ 'check-filter': true, 'active': checked }
+              "@click="checked = !checked"
+            >
+              <span class="checkbox"></span>
+              <span class="check-filter-title">{{ title }}</span>
+            </div>
+          `
+        }
+      }
+    }
+  }
+});
